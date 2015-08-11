e5efe2ec386ffd8a3f0a578b9f0b39b14f54e3a6
Ivan <iferrera@moddity.net>
2015-08-11 17:16:37 +0200

    fix for imprify

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -766,7 +766,8 @@ const int FrontViewPositionNone = 0xff;
 {
     // we could have simply not implemented this, but we choose to call super to make explicit that we
     // want the default behavior.
-    return [super supportedInterfaceOrientations];
+    return UIInterfaceOrientationMaskPortrait;
+
 }
 
 
